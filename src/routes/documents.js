c7c93ae69f68337a064b53414d9f72f78aa80ca3
ryohei tanaka <wellflat@gmail.com>
2018-06-29 23:48:29 +0900

    modified post document handler

--- src/routes/documents.js
+++ src/routes/documents.js
@@ -1,18 +1,35 @@
 import { Router } from 'express';
 import { check, validationResult } from 'express-validator/check';
+import aws from 'aws-sdk';
 import multer from 'multer';
+import multerS3 from 'multer-s3';
+import credential from '../credential.json';
 
 export default ({ config, db }) => {
     const router = Router();
-    const upload = multer({ dest: 'uploads/' });
+    aws.config.update(credential);
+    const s3 = new aws.S3();
+    const upload = multer({
+        storage: multerS3({
+            s3: s3,
+            bucket: config.bucket,
+            contentType: multerS3.AUTO_CONTENT_TYPE,
+            metadata: (req, file, cb) => {
+                cb(null, {fieldName: file.originalname});
+            },
+            key: (req, file, cb) => {
+                cb(null, Date.now().toString());
+            }
+        })
+    });
 
-    router.post('/', upload.any(), async (req, res) => {
+    router.post('/', upload.single('file'), async (req, res) => {
         try {
-            console.log(req.files);
+            console.log(req.file);
             
             res.status(201).json({
                 message: 'file upload success',
-                etag: 1
+                etag: req.file.etag
             });
         } catch (err) {
             console.error(err);
